9424b2a56ca55e9240aa771cd7cadaabdac83a00
Jeff Glanville <jeff.glanville@outlook.com>
2020-01-24 13:43:37 -0600

    render the user form, yup formik and the axios call to add a user to the database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Form from './Components/Form/Form';
+import FormikUserForm from './Components/Form/Form';
 import './App.scss';
 
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <FormikUserForm />
     </div>
   );
 }
--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,13 +1,65 @@
 import React from 'react';
+import {withFormik, Field} from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios';
 
- function Form() {
+ function Form(values, isSubmitting) {
     return (
-        <div>
-            <h1>FormikForm Data</h1>
-        </div>
-    )
+        <form>
+            <div>
+                <Field type = 'name' name = 'name' placeholder = 'Name' />
+
+                <Field type = 'password' name = 'password' placeholder = 'Password' />
+
+                <Field type = 'email' name = 'email' placeholder = 'Email' />
+
+                <Field type = 'checkbox' name = 'tos' checked = {values.tos} />
+                Accept the TOS
+
+                <button>Submit User Info</button>
+            </div>
+        </form>
+    );
 }
 
+const FormikUserForm = withFormik({
+    mapPropsToValues({name, password, email, tos}) {
+        return {
+            name: name || "",
+            password: password || "",
+            email: email || "",
+            tos: tos || false
+        };
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string()
+        .required("Name is required"),
+        password: Yup.string()
+        .min(8, "Password requires 8 characters minimum")
+        .required("Password is required"),
+        email: Yup.string()
+        .email("Email is not valid")
+        .required("Email is required")
+    }),
+    handleSubmit(values, {resetForm, setErrors, setSubmitting}) {
+        if (values.email === "alreadytaken@reqres.in"){
+            setErrors({ email: "That email already in use"});
+        }else {
+            axios
+            .post("https://reqres.in/api/users", values)
+            .then(res => {
+                console.log(res);
+                resetForm();
+                setSubmitting(false);
+            })
+            .catch(err => {
+                console.log(err);
+                setSubmitting(false);
+            });
+        }
+    }
+})(Form)
 
 
-export default Form;
+
+export default FormikUserForm;
